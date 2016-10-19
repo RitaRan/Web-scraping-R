2f8481faa172dd7167475513a3e3f6995833567d
Cecily Abraham <cecily.abraham@duke.edu>
2016-10-19 12:12:52 -0400

    comments

--- get_lq.R
+++ get_lq.R
@@ -1,25 +1,28 @@
 library(xml2)
 library(rvest)
 
-##Gain the mian websie url and saved as site
+#Set site to be the la Quinta website Colin made for us
 site = "http://www2.stat.duke.edu/~cr173/lq_test/www.lq.com/en/findandbook/"
-url = paste0(site,"hotel-listings.html")  ## paste the hotel list website to the url
+#Paste the hotel list website to the La Quinta website
+url = paste0(site,"hotel-listings.html")
 
-page = read_html(url) ## read the data from website  
+#Read the data from website
+page = read_html(url)  
 
-## gained a list includes all hotel html information
-hotel_pages = page %>% 
-  html_nodes("#hotelListing .col-sm-12 a")%>%
-  html_attr("href") %>%
+#Got a list of all hotel html information 
+hotel_pages = page %>% #Set hotel_pages to be all the data read in from the website
+  html_nodes("#hotelListing .col-sm-12 a")%>% #Selected the nodes containing the hotel listing info
+  html_attr("href") %>% #Selected the html attributes from the nodes
   na.omit() 
 
-## created a file to save the hotel html data
+#Created a file to save the hotel html data
 dir.create("data/lq",recursive = TRUE,showWarnings = FALSE)
 
-for(hotel_page in hotel_pages) ## loop for specefic hotel page in the list
+## loop for specific hotel page in the list
+for(hotel_page in hotel_pages) 
 {
   hotel_url = paste0(site, hotel_page)  ## gain the hotel url of each hotel
-  ##download hotel files and saved in to data/lq fold
+  ##download hotel files and save in "data/lq" file
   download.file(url = hotel_url, 
                 destfile = file.path("data/lq", hotel_page),
                 quiet = TRUE) 
