51b2aaab5b701d68c2ef99467539ad163b8a49f4
Ting Dai <td104@saxon.stat.duke.edu>
2016-10-19 01:51:45 -0400

    add the write up

--- get_lq.R
+++ get_lq.R
@@ -1,23 +1,27 @@
 library(xml2)
 library(rvest)
 
+##Gain the mian websie url and saved as site
 site = "http://www2.stat.duke.edu/~cr173/lq_test/www.lq.com/en/findandbook/"
-url = paste0(site,"hotel-listings.html")
+url = paste0(site,"hotel-listings.html")  ## paste the hotel list website to the url
 
-page = read_html(url)
+page = read_html(url) ## read the data from website  
 
+## gained a list includes all hotel html information
 hotel_pages = page %>% 
   html_nodes("#hotelListing .col-sm-12 a")%>%
   html_attr("href") %>%
   na.omit() 
 
+## created a file to save the hotel html data
 dir.create("data/lq",recursive = TRUE,showWarnings = FALSE)
 
-for(hotel_page in hotel_pages)
+for(hotel_page in hotel_pages) ## loop for specefic hotel page in the list
 {
-  hotel_url = paste0(site, hotel_page)
+  hotel_url = paste0(site, hotel_page)  ## gain the hotel url of each hotel
+  ##download hotel files and saved in to data/lq fold
   download.file(url = hotel_url, 
                 destfile = file.path("data/lq", hotel_page),
-                quiet = TRUE)
+                quiet = TRUE) 
 }
 
--- parse_lq.R
+++ parse_lq.R
@@ -5,56 +5,57 @@ library(tibble)
 library(dplyr)
 
 files = dir("data/lq", "html", full.names = TRUE)
-
+## generated a list called res to save the result
 res = list()
 
-for(i in seq_along(files)){
+
+for(i in seq_along(files)){ 
   
-  page = read_html(files[i])
+  page = read_html(files[i]) ##gained the html link for each LQ hotel
   
   hotel_info = page %>% 
-    html_nodes(".hotelDetailsBasicInfoTitle p") %>%
-    html_text() %>%
-    str_split("\n") %>%
-    .[[1]] %>%
-    str_trim() %>%
-    .[.!=""]
-  
-  loc_name = page %>%
+    html_nodes(".hotelDetailsBasicInfoTitle p") %>% ## select the nodes to gain a vector containing the basic hotel information
+    html_text() %>% ## gained the text information
+    str_split("\n") %>% ## split the string, to gain each object seperately. 
+    .[[1]] %>%        ## gained the first object
+    str_trim() %>%  ## deleted the empty object
+    .[.!=""]     ##deleted the empty space
+   
+  loc_name = page %>% ## gained the hotel names
     html_nodes("h1") %>%
     html_text()
   
-  long_lat = page %>%
+  long_lat = page %>% ## gained the longitude from the url of hotel map picutre
     html_nodes(".minimap") %>%
     html_attr("src") %>%
-    str_match("\\|(-*[0-9]{1,3}\\.[0-9]*),(-*[0-9]{1,3}\\.[0-9]*)&")
+    str_match("\\|(-*[0-9]{1,3}\\.[0-9]*),(-*[0-9]{1,3}\\.[0-9]*)&") ## selected the longitude and latitude
   
-  floor = page %>%
+  floor = page %>%  ## gained the floor information of the hotel
     html_nodes(".hotelFeatureList li:nth-child(1)") %>%
     html_text() %>%
     str_trim() %>%
     str_replace("Floors: ","") %>%
     as.integer()
   
-  n_rooms = page %>%
+  n_rooms = page %>% ## gained the room number information 
     html_nodes(".hotelFeatureList li:nth-child(2)") %>%
     html_text() %>%
     str_trim() %>%
-    str_replace("Rooms: ","") %>%
+    str_replace("Rooms: ","") %>% ## keep the number and set as integer
     as.integer()
   
-  amenities = page %>%
+  amenities = page %>% 
     html_nodes(".section:nth-child(1) .pptab_contentL li") %>%
     html_text() %>%
     str_trim()
     
-  state = str_match(hotel_info[2],"(\\, )(.*)( .*)")[3]
-  
+  state = str_match(hotel_info[2],"(\\, )(.*)( .*)")[3] ## gained the state information of the hotel  
+  ## hard coding, generage a vector including all state's name 
   States = c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
              "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY",
              "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV",
              "WI", "WY", "GU", "PR", "VI")
-  
+  ## condition on the hotels located in the US,save the hotel data in to res list
   if(state %in% States){
     res[[i]] = data_frame(
       loc_name = loc_name,
@@ -70,8 +71,9 @@ for(i in seq_along(files)){
   }
 }
 
+## Created a data frame including the US LQ hotels
 hotels = bind_rows(res)
-
+## save the hotel data into data fold
 dir.create("data/",showWarnings = FALSE)
 save(hotels, file="data/lq.Rdata")
   
